7c7fec2db01dde32b3f41147ac87eedd1be123f5
armena <armena@YLT160003.corp.optym.net>
2019-06-02 18:38:25 +0400

    update script

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ sudo docker-compose  up -d --build
 # build one microservice
 #mvn clean install -pl auth -am -DskipTests=true
 #deploy one service
-# sudo docker-compose up -d --no-deps --build auth
+# sudo docker-compose up -d --no-deps --build search
 
 
 
